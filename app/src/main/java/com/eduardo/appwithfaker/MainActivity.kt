00e03f3b816694bad6b7a1cc986fb0c30fa3bbb2
UserEdd <eduardodominguez@laselva.edu.mx>
2024-07-09 23:17:49 -0600

    Unidad Deportiva por Eduardo

--- app/src/main/java/com/eduardo/appwithfaker/MainActivity.kt
+++ app/src/main/java/com/eduardo/appwithfaker/MainActivity.kt
@@ -68,6 +68,8 @@ class MainActivity : ComponentActivity(), OnMapReadyCallback {
         val locations = listOf(
             LatLng(16.8958594, -92.0672737),
             LatLng(16.8777072, -92.1068749)
+            // Agregado por Eduardo Antonio
+            LatLng(16.90382048532684, -92.08386172114714)
         )
 
         for (location in locations) {
